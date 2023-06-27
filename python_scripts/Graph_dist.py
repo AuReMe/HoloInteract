3e37845a61503e71d74afca04ccf52f220130886
CoLucas22 <92378210+CoLucas22@users.noreply.github.com>
2023-06-27 10:03:12 +0200

    modif

--- python_scripts/Graph_dist.py
+++ python_scripts/Graph_dist.py
@@ -211,7 +211,7 @@ def plot_regression_all(input_file, output, base_file, show_points=False, correc
                 writer.writerow(
                     [couple_name, slope, spearman_coef, spearman_p])
                 counter += 1
-                if spearman_p < (0.05) and slope < 0:
+                if (spearman_p < (0.05) or spearman_p == "nan") and slope < 0:
                     # Plot de la droite de régression et des points correspondants
                     fig.add_trace(go.Scatter(x=x_data, y=y_pred, name=couple_name, line=dict(
                         color=colors[counter % len(colors)])))
